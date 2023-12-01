818376b9ba504f784ac702db69519a52b08a02bc
Chayutp <135034854+Chayutp@users.noreply.github.com>
2023-12-01 18:07:46 +0700

    feat(js-loop)phum

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,8 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
-let minNumber;
+let minNumber = numbers[0];
 // Start coding here
-
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < minNumber) minNumber = numbers[i];
+}
 console.log(minNumber);
