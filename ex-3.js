1783766f835cdfc8ab8732fada3191804fd42658
Havocrazer <92991759+Havocrazer@users.noreply.github.com>
2023-12-01 14:14:43 +0700

    feat: add ex1-6

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
-let minNumber;
-// Start coding here
-
+let minNumber = numbers[0];
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] < minNumber) minNumber = numbers[i];
+}
 console.log(minNumber);
--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,11 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
+// let numbers = [1, 2, 3, 4, 5, 6];
 
-// Start coding here
+// for (let i = 0; i < companyName.length && i < numbers.length; i++) {
+//   console.log("Number " + numbers[i] + " Character is " + companyName[i]);
+// }
+
+for (let i = 0; i < companyName.length; i++) {
+  console.log("Number " + (i + 1) + " Character is " + companyName[i]);
+}
