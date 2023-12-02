9385ecf99e594fa144e68716a30fa47f1759665e
Am <arphasirisubb@gmail.com>
2023-12-02 11:52:27 +0700

    submit loops assignment

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,16 @@
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
-let minNumber;
+let minNumber; 
 // Start coding here
 
+for (let i = 0; i < numbers.length; i++){
+    if(i === 0){
+        minNumber = numbers[i]
+    } else if (numbers[i] < minNumber){
+        minNumber = numbers[i]
+}
+}
+    
+
 console.log(minNumber);
+
